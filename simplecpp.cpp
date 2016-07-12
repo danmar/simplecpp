367feab3414358231208746fda4a8e537084a618
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2016-07-12 19:39:55 +0200

    Revert "Added filename for tokenlist"
    This reverts commit ddbd3a384451347322fbc2c1e6e9dee53a041350.

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -82,14 +82,14 @@ bool simplecpp::Token::endsWithOneOf(const char c[]) const {
     return std::strchr(c, str[str.size() - 1U]) != 0;
 }
 
-simplecpp::TokenList::TokenList(std::vector<std::string> &filenames) : first(nullptr), last(nullptr), files(filenames), fileName_("") {}
+simplecpp::TokenList::TokenList(std::vector<std::string> &filenames) : first(nullptr), last(nullptr), files(filenames) {}
 
 simplecpp::TokenList::TokenList(std::istream &istr, std::vector<std::string> &filenames, const std::string &filename, OutputList *outputList)
-    : first(nullptr), last(nullptr), files(filenames), fileName_(filename) {
-    readfile(istr,filename, outputList);
+    : first(nullptr), last(nullptr), files(filenames) {
+    readfile(istr,filename,outputList);
 }
 
-simplecpp::TokenList::TokenList(const TokenList &other) : first(nullptr), last(nullptr), files(other.files), fileName_(other.fileName_) {
+simplecpp::TokenList::TokenList(const TokenList &other) : first(nullptr), last(nullptr), files(other.files) {
     *this = other;
 }
 
@@ -101,7 +101,6 @@ void simplecpp::TokenList::operator=(const TokenList &other) {
     if (this == &other)
         return;
     clear();
-    fileName_ = other.fileName_;
     for (const Token *tok = other.cbegin(); tok; tok = tok->next)
         push_back(new Token(*tok));
 }
@@ -113,7 +112,6 @@ void simplecpp::TokenList::clear() {
         first = next;
     }
     last = nullptr;
-    fileName_.clear();
 }
 
 void simplecpp::TokenList::push_back(Token *tok) {
@@ -151,9 +149,6 @@ std::string simplecpp::TokenList::stringify() const {
 
 void simplecpp::TokenList::readfile(std::istream &istr, const std::string &filename, OutputList *outputList)
 {
-    clear();
-    fileName_ = filename;
-
     std::stack<simplecpp::Location> loc;
 
     unsigned int multiline = 0U;
@@ -615,7 +610,7 @@ public:
     explicit Macro(const std::string &name, const std::string &value, std::vector<std::string> &f) : nameToken(nullptr), files(f), tokenListDefine(f) {
         const std::string def(name + ' ' + value);
         std::istringstream istr(def);
-        tokenListDefine.readfile(istr,"");
+        tokenListDefine.readfile(istr);
         parseDefine(tokenListDefine.cbegin());
     }
 
