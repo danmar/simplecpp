b5ee37aadd43bc44234cadb10d59a72d0347d43a
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2016-07-22 14:54:09 +0200

    improved test output, write number of tests/skipped/todo/failed

--- run-clang-tests.py
+++ run-clang-tests.py
@@ -14,73 +14,6 @@ def cleanup(out):
 
 commands = []
 for f in sorted(glob.glob(os.path.expanduser('~/llvm/tools/clang/test/Preprocessor/*.c*'))):
-  filename = f[f.rfind('/')+1:]
-
-  # skipping tests..
-  skip = ['assembler-with-cpp.c',
-          'builtin_line.c',
-          'has_attribute.c',
-          'line-directive-output.c',
-          'microsoft-ext.c',
-          '_Pragma-location.c',
-          '_Pragma-dependency.c',
-          '_Pragma-dependency2.c',
-          '_Pragma-physloc.c',
-          'pragma-pushpop-macro.c', # pragma push/pop
-          'x86_target_features.c',
-          'warn-disabled-macro-expansion.c']
-
-  if filename in skip:
-    continue
-
-  todo = [
-           # todo, low priority: wrong number of macro arguments, pragma, etc
-           'header_lookup1.c',
-           'macro_backslash.c',
-           'macro_fn_comma_swallow.c',
-           'macro_fn_comma_swallow2.c',
-           'macro_fn_lparen_scan.c',
-           'macro_fn_lparen_scan2.c',
-           'macro_disable.c',
-           'macro_expand.c',
-           'macro_expand_empty.c',
-           'macro_fn_disable_expand.c',
-           'macro_paste_commaext.c',
-           'macro_paste_empty.c',
-           'macro_paste_hard.c',
-           'macro_paste_hashhash.c',
-           'macro_paste_identifier_error.c',
-           'macro_paste_simple.c',
-           'macro_paste_spacing.c',
-           'macro_rescan.c',
-           'macro_rescan_varargs.c',
-           'macro_rescan2.c',
-           'macro_space.c',
-
-  # todo, high priority
-           'c99-6_10_3_3_p4.c',
-           'c99-6_10_3_4_p5.c',
-           'c99-6_10_3_4_p6.c',
-           'comment_save.c',
-           'cxx_and.cpp', # if A and B
-           'cxx_bitand.cpp',
-           'cxx_bitor.cpp', # if A bitor B
-           'cxx_compl.cpp',  # if A compl B
-           'cxx_not.cpp',
-           'cxx_not_eq.cpp',  # if A not_eq B
-           'cxx_or.cpp',  # if A or B
-           'cxx_xor.cpp',  # if A xor B
-           'cxx_oper_keyword_ms_compat.cpp',
-           'expr_usual_conversions.c', # condition is true: 4U - 30 >= 0
-           'hash_line.c',
-           'macro_fn_varargs_named.c',  # named vararg arguments
-           'mi_opt2.c',  # stringify
-           'print_line_include.c',  #stringify
-           'stdint.c',
-           'stringize_misc.c']
-
-  if filename in todo:
-    continue
 
   for line in open(f, 'rt'):
     if line.startswith('// RUN: %clang_cc1 '):
@@ -91,7 +24,75 @@ for f in sorted(glob.glob(os.path.expanduser('~/llvm/tools/clang/test/Preprocess
       if len(cmd) > 1:
         commands.append(cmd[1:] + ' ' + f)
 
+# skipping tests..
+skip = ['assembler-with-cpp.c',
+        'builtin_line.c',
+        'has_attribute.c',
+        'line-directive-output.c',
+        'microsoft-ext.c',
+        '_Pragma-location.c',
+        '_Pragma-dependency.c',
+        '_Pragma-dependency2.c',
+        '_Pragma-physloc.c',
+        'pragma-pushpop-macro.c', # pragma push/pop
+        'x86_target_features.c',
+        'warn-disabled-macro-expansion.c']
+
+todo = [
+         # todo, low priority: wrong number of macro arguments, pragma, etc
+         'header_lookup1.c',
+         'macro_backslash.c',
+         'macro_fn_comma_swallow.c',
+         'macro_fn_comma_swallow2.c',
+         'macro_fn_lparen_scan.c',
+         'macro_fn_lparen_scan2.c',
+         'macro_disable.c',
+         'macro_expand.c',
+         'macro_expand_empty.c',
+         'macro_fn_disable_expand.c',
+         'macro_paste_commaext.c',
+         'macro_paste_empty.c',
+         'macro_paste_hard.c',
+         'macro_paste_hashhash.c',
+         'macro_paste_identifier_error.c',
+         'macro_paste_simple.c',
+         'macro_paste_spacing.c',
+         'macro_rescan.c',
+         'macro_rescan_varargs.c',
+         'macro_rescan2.c',
+         'macro_space.c',
+
+         # todo, high priority
+         'c99-6_10_3_3_p4.c',
+         'c99-6_10_3_4_p5.c',
+         'c99-6_10_3_4_p6.c',
+         'comment_save.c',
+         'cxx_and.cpp', # if A and B
+         'cxx_bitand.cpp',
+         'cxx_bitor.cpp', # if A bitor B
+         'cxx_compl.cpp',  # if A compl B
+         'cxx_not.cpp',
+         'cxx_not_eq.cpp',  # if A not_eq B
+         'cxx_or.cpp',  # if A or B
+         'cxx_xor.cpp',  # if A xor B
+         'cxx_oper_keyword_ms_compat.cpp',
+         'expr_usual_conversions.c', # condition is true: 4U - 30 >= 0
+         'hash_line.c',
+         'macro_fn_varargs_named.c',  # named vararg arguments
+         'mi_opt2.c',  # stringify
+         'print_line_include.c',  #stringify
+         'stdint.c',
+         'stringize_misc.c']
+
+numberOfSkipped = 0
+numberOfFailed = 0
+numberOfTodos = 0
+
 for cmd in set(commands):
+  if cmd[cmd.rfind('/')+1:] in skip:
+    numberOfSkipped = numberOfSkipped + 1
+    continue
+
   clang_cmd = ['clang']
   clang_cmd.extend(cmd.split(' '))
   p = subprocess.Popen(clang_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -105,9 +106,16 @@ for cmd in set(commands):
   cppcheck_output = comm[0]
 
   if cleanup(clang_output) != cleanup(cppcheck_output):
-    print(cmd)
-    print(cmd[cmd.rfind('/')+1:])
-    #print('clang_output:\n' + cleanup(clang_output))
-    #print('cppcheck_output:\n' + cleanup(cppcheck_output))
-    #break
+    filename = cmd[cmd.rfind('/')+1:]
+    if filename in todo:
+      print('TODO ' + cmd)
+      numberOfTodos = numberOfTodos + 1
+    else:
+      print('FAILED ' + cmd)
+      numberOfFailed = numberOfFailed + 1
+
+print('Number of tests: ' + str(len(commands)))
+print('Number of skipped: ' + str(numberOfSkipped))
+print('Number of todos: ' + str(numberOfTodos))
+print('Number of failed: ' + str(numberOfFailed))
 
