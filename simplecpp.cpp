c55799c37ec5662d45c784ae053ca2b579ee3c69
Alexander Mai <amai@users.sf.net>
2017-05-07 19:25:31 +0200

    Small refactoring

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -124,6 +124,39 @@ namespace {
                 (!tok->previous || tok->previous->op == '(') &&
                 (tok->next && (tok->next->name || tok->next->number)));
     }
+
+    /*! Helper class to aid in the initializing global const data */
+    template < typename Cont >
+    class make_container {
+    public:
+        typedef make_container< Cont > my_type;
+        typedef typename Cont::value_type T;
+
+        my_type& operator<< (const T& val) {
+            data_.insert(data_.end(), val);
+            return *this;
+        }
+        my_type& operator<< (const Cont& other_container) {
+            for (typename Cont::const_iterator it=other_container.begin(); it!=other_container.end(); ++it) {
+                data_.insert(data_.end(), *it);
+            }
+            return *this;
+        }
+        my_type& operator<< (T&& val) {
+            data_.insert(data_.end(), val);
+            return *this;
+        }
+        my_type& operator<< (const char* val) {
+            data_.insert(data_.end(), val);
+            return *this;
+        }
+        operator Cont() const {
+            return data_;
+        }
+    private:
+        Cont data_;
+    };
+
 }
 
 void simplecpp::Location::adjust(const std::string &str)
@@ -1737,16 +1770,16 @@ namespace {
         }
     }
 
+    const std::set<std::string> altop = make_container< std::set<std::string> >() <<
+                                        "and" <<
+                                        "or" <<
+                                        "bitand" <<
+                                        "bitor" <<
+                                        "not" <<
+                                        "not_eq" <<
+                                        "xor";
     void simplifyName(simplecpp::TokenList &expr)
     {
-        std::set<std::string> altop;
-        altop.insert("and");
-        altop.insert("or");
-        altop.insert("bitand");
-        altop.insert("bitor");
-        altop.insert("not");
-        altop.insert("not_eq");
-        altop.insert("xor");
         for (simplecpp::Token *tok = expr.front(); tok; tok = tok->next) {
             if (tok->name) {
                 if (altop.find(tok->str) != altop.end()) {
