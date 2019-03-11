danmar/simplecpp

commit 4e022f0a8d7c2a669260e1a0f93dc27d4bad702d

Merge string and character literal with prefix (#155)
Previously, simplecpp would output the character L'a' as two tokens "L"
and "'a'". A user of the tokens has no way of knowing if they were
originally "L'a'" och "L 'a'".

test.cpp
@@ -1354,17 +1354,36 @@ static void readfile_nullbyte()
     ASSERT_EQUALS(true, outputList.empty()); // should warning be written?
 }
 
+static void readfile_char()
+{
+    ASSERT_EQUALS("'c'", readfile("'c'"));
+    ASSERT_EQUALS("L'c'", readfile("L'c'"));
+    ASSERT_EQUALS("L 'c'", readfile("L 'c'"));
+    ASSERT_EQUALS("A = 'c'", readfile("A = 'c'"));
+    ASSERT_EQUALS("A = L'c'", readfile("A = L'c'"));
+    ASSERT_EQUALS("A = u'c'", readfile("A = u'c'"));
+    ASSERT_EQUALS("A = U'c'", readfile("A = U'c'"));
+    ASSERT_EQUALS("A = u8'c'", readfile("A = u8'c'"));
+    ASSERT_EQUALS("A = u8 'c'", readfile("A = u8 'c'"));
+}
+
 static void readfile_string()
 {
-    const char code[] = "A = \"abc\'def\"";
-    ASSERT_EQUALS("A = \"abc\'def\"", readfile(code));
+    ASSERT_EQUALS("\"\"", readfile("\"\""));
+    ASSERT_EQUALS("\"a\"", readfile("\"a\""));
+    ASSERT_EQUALS("u\"a\"", readfile("u\"a\""));
+    ASSERT_EQUALS("u \"a\"", readfile("u \"a\""));
+    ASSERT_EQUALS("A = \"\"", readfile("A = \"\""));
+    ASSERT_EQUALS("A = \"abc\'def\"", readfile("A = \"abc\'def\""));
     ASSERT_EQUALS("( \"\\\\\\\\\" )", readfile("(\"\\\\\\\\\")"));
     ASSERT_EQUALS("x = \"a  b\"\n;", readfile("x=\"a\\\n  b\";"));
     ASSERT_EQUALS("x = \"a  b\"\n;", readfile("x=\"a\\\r\n  b\";"));
-}
 
-static void readfile_rawstring()
-{
+    ASSERT_EQUALS("A = u8\"a\"", readfile("A = u8\"a\""));
+    ASSERT_EQUALS("A = u\"a\"", readfile("A = u\"a\""));
+    ASSERT_EQUALS("A = U\"a\"", readfile("A = U\"a\""));
+    ASSERT_EQUALS("A = L\"a\"", readfile("A = L\"a\""));
+    ASSERT_EQUALS("A = L \"a\"", readfile("A = L \"a\""));
     ASSERT_EQUALS("A = \"abc\\\\\\\\def\"", readfile("A = R\"(abc\\\\def)\""));
     ASSERT_EQUALS("A = \"abc\\\\\\\\def\"", readfile("A = R\"x(abc\\\\def)x\""));
     ASSERT_EQUALS("A = \"\"", readfile("A = R\"()\""));
@@ -1372,10 +1391,10 @@ static void readfile_rawstring()
     ASSERT_EQUALS("A = \"\\\"\"", readfile("A = R\"(\")\""));
     ASSERT_EQUALS("A = \"abc\"", readfile("A = R\"\"\"(abc)\"\"\""));
     ASSERT_EQUALS("A = \"a\nb\nc\";", readfile("A = R\"foo(a\nb\nc)foo\";"));
-    ASSERT_EQUALS("A = L \"abc\"", readfile("A = LR\"(abc)\""));
-    ASSERT_EQUALS("A = u \"abc\"", readfile("A = uR\"(abc)\""));
-    ASSERT_EQUALS("A = U \"abc\"", readfile("A = UR\"(abc)\""));
-    ASSERT_EQUALS("A = u8 \"abc\"", readfile("A = u8R\"(abc)\""));
+    ASSERT_EQUALS("A = L\"abc\"", readfile("A = LR\"(abc)\""));
+    ASSERT_EQUALS("A = u\"abc\"", readfile("A = uR\"(abc)\""));
+    ASSERT_EQUALS("A = U\"abc\"", readfile("A = UR\"(abc)\""));
+    ASSERT_EQUALS("A = u8\"abc\"", readfile("A = u8R\"(abc)\""));
 }
 
 static void readfile_cpp14_number()
@@ -1786,8 +1805,8 @@ int main(int argc, char **argv)
     TEST_CASE(multiline7); // multiline string in macro
 
     TEST_CASE(readfile_nullbyte);
+    TEST_CASE(readfile_char);
     TEST_CASE(readfile_string);
-    TEST_CASE(readfile_rawstring);
     TEST_CASE(readfile_cpp14_number);
     TEST_CASE(readfile_unhandled_chars);
     TEST_CASE(readfile_error);

simplecpp.cpp
@@ -327,6 +327,15 @@ static void ungetChar(std::istream &istr, unsigned int bom)
         istr.unget();
 }
 
+static unsigned char prevChar(std::istream &istr, unsigned int bom)
+{
+    ungetChar(istr, bom);
+    ungetChar(istr, bom);
+    unsigned char c = readChar(istr, bom);
+    readChar(istr, bom);
+    return c;
+}
+
 static unsigned short getAndSkipBOM(std::istream &istr)
 {
     const int ch1 = istr.peek();
@@ -384,9 +393,10 @@ static void portabilityBackslash(simplecpp::OutputList *outputList, const std::v
     outputList->push_back(err);
 }
 
-static bool isRawStringId(const std::string &str)
+static bool isStringLiteralPrefix(const std::string &str)
 {
-    return str == "R" || str == "uR" || str == "UR" || str == "LR" || str == "u8R";
+    return str == "u" || str == "U" || str == "L" || str == "u8" ||
+           str == "R" || str == "uR" || str == "UR" || str == "LR" || str == "u8R";
 }
 
 void simplecpp::TokenList::readfile(std::istream &istr, const std::string &filename, OutputList *outputList)
@@ -545,31 +555,34 @@ void simplecpp::TokenList::readfile(std::istream &istr, const std::string &filen
 
         // string / char literal
         else if (ch == '\"' || ch == '\'') {
-            // C++11 raw string literal
-            if (ch == '\"' && cback() && cback()->name && isRawStringId(cback()->str())) {
+            if (cback() && cback()->name && !std::isspace(prevChar(istr, bom)) && (isStringLiteralPrefix(cback()->str()))) {
                 std::string delim;
-                ch = readChar(istr,bom);
-                while (istr.good() && ch != '(' && ch != '\n') {
-                    delim += ch;
+                currentToken = ch;
+                bool hasR = *cback()->str().rbegin() == 'R';
+                std::string prefix = cback()->str();
+                if (hasR) {
+                    prefix.resize(prefix.size() - 1);
+                    delim = ")";
                     ch = readChar(istr,bom);
+                    while (istr.good() && ch != '(' && ch != '\n') {
+                        delim += ch;
+                        ch = readChar(istr,bom);
+                    }
+                    if (!istr.good() || ch == '\n')
+                        // TODO report
+                        return;
                 }
-                if (!istr.good() || ch == '\n')
-                    // TODO report
-                    return;
-                currentToken = '\"';
-                const std::string endOfRawString(')' + delim + '\"');
-                while (istr.good() && !endsWith(currentToken, endOfRawString))
+                const std::string endOfRawString(delim + currentToken);
+                while (istr.good() && !(endsWith(currentToken, endOfRawString) && currentToken.size() > 1))
                     currentToken += readChar(istr,bom);
                 if (!endsWith(currentToken, endOfRawString))
                     // TODO report
                     return;
                 currentToken.erase(currentToken.size() - endOfRawString.size(), endOfRawString.size() - 1U);
-                if (cback()->op == 'R')
-                    back()->setstr(escapeString(currentToken));
-                else {
-                    back()->setstr(cback()->str().substr(0, cback()->str().size() - 1));
-                    push_back(new Token(currentToken, location)); // push string without newlines
-                }
+                if (hasR)
+                    currentToken = escapeString(currentToken);
+                currentToken.insert(0, prefix);
+                back()->setstr(currentToken);
                 location.adjust(currentToken);
                 if (currentToken.find_first_of("\r\n") == std::string::npos)
                     location.col += 2 + 2 * delim.size();

commit eb631e60dc6dd55ae7eea1c6e04a4a7ae4ed0822

Add regression tests for string literals with " inside
Commit 4e022f0a8d7c2a669260e1a0f93dc27d4bad702d broke (among other
things), parsing of prefixed strings with \" and prefixed characters
containing \'. This (and other things) was fixed in
1eb30dcea852431f3e355383534ea527e48c0b5d. Add tests for this.

test.cpp
@@ -1381,6 +1381,13 @@ static void readfile_char()
     ASSERT_EQUALS("A = U'c'", readfile("A = U'c'"));
     ASSERT_EQUALS("A = u8'c'", readfile("A = u8'c'"));
     ASSERT_EQUALS("A = u8 'c'", readfile("A = u8 'c'"));
+
+    // The character \'
+    ASSERT_EQUALS("'\\''", readfile("'\\\''"));
+    ASSERT_EQUALS("L'\\''", readfile("L'\\\''"));
+    ASSERT_EQUALS("u'\\''", readfile("u'\\\''"));
+    ASSERT_EQUALS("U'\\''", readfile("U'\\\''"));
+    ASSERT_EQUALS("u8'\\''", readfile("u8'\\\''"));
 }
 
 static void readfile_char_error()
@@ -1423,6 +1430,18 @@ static void readfile_string()
     ASSERT_EQUALS("A = u\"abc\"", readfile("A = uR\"(abc)\""));
     ASSERT_EQUALS("A = U\"abc\"", readfile("A = UR\"(abc)\""));
     ASSERT_EQUALS("A = u8\"abc\"", readfile("A = u8R\"(abc)\""));
+
+    // Strings containing \"
+    ASSERT_EQUALS("\"\\\"\"", readfile("\"\\\"\""));
+    ASSERT_EQUALS("L\"\\\"\"", readfile("L\"\\\"\""));
+    ASSERT_EQUALS("u\"\\\"\"", readfile("u\"\\\"\""));
+    ASSERT_EQUALS("U\"\\\"\"", readfile("U\"\\\"\""));
+    ASSERT_EQUALS("u8\"\\\"\"", readfile("u8\"\\\"\""));
+    ASSERT_EQUALS("\"\\\"a\\\"\"", readfile("\"\\\"a\\\"\""));
+    ASSERT_EQUALS("L\"a\\\"\\\"\"", readfile("L\"a\\\"\\\"\""));
+    ASSERT_EQUALS("u\"a\\\"\\\"\"", readfile("u\"a\\\"\\\"\""));
+    ASSERT_EQUALS("U\"a\\\"\\\"\"", readfile("U\"a\\\"\\\"\""));
+    ASSERT_EQUALS("u8\"a\\\"\\\"\"", readfile("u8\"a\\\"\\\"\""));
 }
 
 static void readfile_string_error()