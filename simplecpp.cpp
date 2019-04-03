0c44ecb22eb9819025993b0258152eb5ad81122c
firewave <firewave@users.noreply.github.com>
2019-04-03 20:08:13 +0200

    optimized simplecpp::Location::adjust() by using strpbrk() instead of std::string::find_first_of()

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -155,7 +155,7 @@ const std::string simplecpp::Location::emptyFileName;
 
 void simplecpp::Location::adjust(const std::string &str)
 {
-    if (str.find_first_of("\r\n") == std::string::npos) {
+    if (strpbrk(str.c_str(), "\r\n") == nullptr) {
         col += str.size();
         return;
     }
