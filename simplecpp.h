ce476413799027cdae3661449ea87f0c252a916a
firewave <firewave@users.noreply.github.com>
2022-03-22 20:36:02 +0100

    removed unnecessary "static" specifier from functions in simplecpp namespace

--- simplecpp.h
+++ simplecpp.h
@@ -353,10 +353,10 @@ namespace simplecpp {
     SIMPLECPP_LIB std::string convertCygwinToWindowsPath(const std::string &cygwinPath);
 
     /** Returns the __STDC_VERSION__ value for a given standard */
-    SIMPLECPP_LIB static std::string getCStdString(const std::string &std);
+    SIMPLECPP_LIB std::string getCStdString(const std::string &std);
 
     /** Returns the __cplusplus value for a given standard */
-    SIMPLECPP_LIB static std::string getCppStdString(const std::string &std);
+    SIMPLECPP_LIB std::string getCppStdString(const std::string &std);
 }
 
 #if (__cplusplus < 201103L) && !defined(__APPLE__)
