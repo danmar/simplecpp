4f6415f864b27dc724095784fe4b3505e6b2fc85
firewave <firewave@users.noreply.github.com>
2023-03-03 10:40:47 +0100

    test.cpp: run tests with filename input

--- test.cpp
+++ test.cpp
@@ -20,7 +20,8 @@
 
 enum Input {
     Stringstream,
-    Fstream
+    Fstream,
+    File
 };
 
 static Input USE_INPUT = Stringstream;
@@ -46,6 +47,8 @@ static const char* inputString(Input input) {
             return "Stringstream";
         case Fstream:
             return "Fstream";
+        case File:
+            return "File";
     }
 }
 
@@ -106,6 +109,14 @@ static simplecpp::TokenList makeTokenListFromFstream(const char code[], std::siz
     return tokenList;
 }
 
+static simplecpp::TokenList makeTokenListFromFile(const char code[], std::size_t size, std::vector<std::string> &filenames, const std::string &filename, simplecpp::OutputList *outputList)
+{
+    const std::string tmpfile = writeFile(code, size, filename);
+    simplecpp::TokenList tokenList(tmpfile, filenames, outputList);
+    remove(tmpfile.c_str());
+    return tokenList;
+}
+
 static simplecpp::TokenList makeTokenList(const char code[], std::size_t size, std::vector<std::string> &filenames, const std::string &filename=std::string(), simplecpp::OutputList *outputList=nullptr)
 {
     switch (USE_INPUT) {
@@ -115,6 +126,8 @@ static simplecpp::TokenList makeTokenList(const char code[], std::size_t size, s
         }
         case Fstream:
             return makeTokenListFromFstream(code, size, filenames, filename, outputList);
+        case File:
+            return makeTokenListFromFile(code, size, filenames, filename, outputList);
     }
 }
 
@@ -3004,5 +3017,6 @@ int main(int argc, char **argv)
 {
     runTests(argc, argv, Stringstream);
     runTests(argc, argv, Fstream);
+    runTests(argc, argv, File);
     return numberOfFailedAssertions > 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
