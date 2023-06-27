bd0cdf076aec3a60db7ff83125f68b9699903239
Edo <edoardo.sanguineti222@gmail.com>
2023-06-27 11:12:26 +0200

    use MSVC specific macro instead of arbitrary win32 macro

--- simplecpp.h
+++ simplecpp.h
@@ -44,7 +44,7 @@
 #define nullptr NULL
 #endif
 
-#if defined(_WIN32)
+#if defined(_MSC_VER)
 // suppress warnings about "conversion from 'type1' to 'type2', possible loss of data"
 #  pragma warning(disable : 4267)
 #  pragma warning(disable : 4244)
