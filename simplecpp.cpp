4a541c9fedb0b171e613ed30968c66ce4a3948f9
firewave <firewave@users.noreply.github.com>
2023-09-12 15:25:31 +0200

    added `FileStreamBuffered` to reduce the amount of `fgetc()` calls [skip ci]

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -60,6 +60,12 @@ using mode_t = unsigned short;
 #  include <sys/types.h>
 #endif
 
+#ifdef __GNUC__
+#  define unlikely(x) __builtin_expect(!!(x), 0)
+#else
+#  define unlikely(x) (x)
+#endif
+
 static bool isHex(const std::string &s)
 {
     return s.size()>2 && (s.compare(0,2,"0x")==0 || s.compare(0,2,"0X")==0);
@@ -469,6 +475,74 @@ private:
     int lastStatus{};
 };
 
+class FileStreamBuffered : public simplecpp::TokenList::Stream {
+public:
+    FileStreamBuffered(const std::string &filename, std::vector<std::string> &files)
+        : file(fopen(filename.c_str(), "rb"))
+    {
+        if (!file) {
+            files.push_back(filename);
+            throw simplecpp::Output(simplecpp::Output::FILE_NOT_FOUND, simplecpp::Location(files), "File is missing: " + filename);
+        }
+        init();
+    }
+
+    ~FileStreamBuffered() override {
+        fclose(file);
+        file = nullptr;
+    }
+
+    FileStreamBuffered(const FileStreamBuffered&) = delete;
+    FileStreamBuffered &operator=(const FileStreamBuffered&) = delete;
+
+    int get() override {
+        read_internal();
+        return buf[buf_idx++];
+    }
+    int peek() override {
+        read_internal();
+        return buf[buf_idx];
+    }
+    void unget() override {
+        --buf_idx;
+    }
+    bool good() override {
+        return lastStatus != EOF;
+    }
+
+private:
+    void read_internal() {
+        // check if we are in the last chunk
+        if (unlikely(buf_idx >= buf_len)) {
+            if (buf_len != sizeof(buf)) {
+                lastStatus = EOF;
+                return;
+            }
+        }
+
+        if (unlikely(buf_idx == -1 || buf_idx == buf_len))
+        {
+            buf_idx = 0;
+            buf_len = fread(buf, 1, sizeof(buf), file);
+            if (buf_len == 0) {
+                lastStatus = EOF;
+            }
+            else if (buf_len != sizeof(buf)) {
+                if (ferror(file)) {
+                    // TODO: is this correct?
+                    lastStatus = EOF;
+                }
+            }
+        }
+    }
+
+    FILE *file;
+    int lastStatus{};
+    unsigned char buf[8192];
+    int buf_len{};
+    int buf_idx{-1};
+};
+
 simplecpp::TokenList::TokenList(std::vector<std::string> &filenames) : frontToken(nullptr), backToken(nullptr), files(filenames) {}
 
 simplecpp::TokenList::TokenList(std::istream &istr, std::vector<std::string> &filenames, const std::string &filename, OutputList *outputList)
@@ -489,7 +563,7 @@ simplecpp::TokenList::TokenList(const std::string &filename, std::vector<std::st
     : frontToken(nullptr), backToken(nullptr), files(filenames)
 {
     try {
-        FileStream stream(filename, filenames);
+        FileStreamBuffered stream(filename, filenames);
         readfile(stream,filename,outputList);
     } catch (const simplecpp::Output & e) { // TODO handle extra type of errors
         outputList->push_back(e);
