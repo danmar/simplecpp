e096d6b3f15f134d37abaa20f12bfbf84b8dd552
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2023-12-22 18:16:20 +0100

    Fix #173 (Change license to 0BSD) (#332)

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -1,19 +1,6 @@
 /*
  * simplecpp - A simple and high-fidelity C/C++ preprocessor library
- * Copyright (C) 2016-2022 Daniel Marjamäki.
- *
- * This library is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2016-2023 simplecpp team
  */
 
 #if defined(_WIN32) || defined(__CYGWIN__) || defined(__MINGW32__)
