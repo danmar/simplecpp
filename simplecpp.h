0174019737c4e801094150fec2ed240c5892ce6c
firewave <firewave@users.noreply.github.com>
2024-02-15 20:40:50 +0100

    added the file/directory existence functions from Cppcheck

--- simplecpp.h
+++ simplecpp.h
@@ -396,6 +396,20 @@ namespace simplecpp {
     SIMPLECPP_LIB std::string getCppStdString(const std::string &std);
     SIMPLECPP_LIB std::string getCppStdString(cppstd_t std);
 
+    /**
+     * @brief Checks if given path is a file
+     * @param path Path to be checked
+     * @return true if given path is a file
+     */
+    SIMPLECPP_LIB bool isFile(const std::string &path);
+
+    /**
+     * @brief Checks if a given path is a directory
+     * @param path Path to be checked
+     * @return true if given path is a directory
+     */
+    SIMPLECPP_LIB bool isDirectory(const std::string &path);
+
     struct SIMPLECPP_LIB FileData {
         /** The canonical filename associated with this data */
         std::string filename;
