15e8427d89b8fcfac171082601c5b6d9a3e5f1de
firewave <firewave@users.noreply.github.com>
2025-06-11 12:40:46 +0200

    main.cpp: replaced `std::exit()` with `return`

--- main.cpp
+++ main.cpp
@@ -81,18 +81,18 @@ int main(int argc, char **argv)
             }
         } else if (filename) {
             std::cout << "error: multiple filenames specified" << std::endl;
-            std::exit(1);
+            return 1;
         } else {
             filename = arg;
         }
     }
 
     if (error)
-        std::exit(1);
+        return 1;
 
     if (quiet && error_only) {
         std::cout << "error: -e cannot be used in conjunction with -q" << std::endl;
-        std::exit(1);
+        return 1;
     }
 
     if (!filename) {
@@ -107,7 +107,7 @@ int main(int argc, char **argv)
         std::cout << "  -is             Use std::istream interface." << std::endl;
         std::cout << "  -e              Output errors only." << std::endl;
         std::cout << "  -f              Fail when errors were encountered (exitcode 1)." << std::endl;
-        std::exit(0);
+        return 0;
     }
 
     dui.removeComments = true;
@@ -120,7 +120,7 @@ int main(int argc, char **argv)
         std::ifstream f(filename);
         if (!f.is_open()) {
             std::cout << "error: could not open file '" << filename << "'" << std::endl;
-            std::exit(1);
+            return 1;
         }
         rawtokens = new simplecpp::TokenList(f, files,filename,&outputList);
     } else {
