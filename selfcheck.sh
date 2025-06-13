a46cb125243f4d6e743eceeba37a485ad131d420
Oliver Stöneberg <firewave@users.noreply.github.com>
2025-06-13 22:33:42 +0200

    selfcheck.sh: actually fail if we encountered unexpected errors (#437)

--- selfcheck.sh
+++ selfcheck.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-output=$(./simplecpp simplecpp.cpp -e 2>&1)
+output=$(./simplecpp simplecpp.cpp -e -f 2>&1)
 ec=$?
-echo "$output" | grep -v 'Header not found: <'
-exit $ec
+errors=$(echo "$output" | grep -v 'Header not found: <')
+if [ $ec -ne 0 ]; then
+  # only fail if got errors which do not refer to missing system includes
+  if [ ! -z "$errors" ]; then
+    exit $ec
+  fi
+fi
