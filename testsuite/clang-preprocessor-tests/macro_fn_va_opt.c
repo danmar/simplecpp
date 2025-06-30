9fd5f63bf23c25df846dbd21b4341f959f114736
glank <glankk@users.noreply.github.com>
2025-06-30 18:36:11 +0200

    Fix __VA_OPT__ handling

---
+++ testsuite/clang-preprocessor-tests/macro_fn_va_opt.c
@@ -0,0 +1,9 @@
+// RUN: %clang_cc1 -E %s | grep '^    printf( "%%s" , "Hello" );$'
+
+#define P( x, ...) printf( x __VA_OPT__(,) __VA_ARGS__ )
+#define PF( x, ...) P( x __VA_OPT__(,) __VA_ARGS__ )
+
+int main()
+{
+    PF( "%s", "Hello" );
+}
