ca3ed55764bff9a3e6ad167ceac2345d230f0be4
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2025-08-25 18:22:20 -0400

    chore: Simplify runastyle.bat by using "@ECHO OFF"
    This change simplifies the Windows batch script by using "@ECHO OFF" to
    reduce unnecessary output, making the script cleaner and easier to read.

--- runastyle.bat
+++ runastyle.bat
@@ -1,25 +1,26 @@
-@REM Script to run AStyle on the sources
-@REM The version check in this script is used to avoid commit battles
-@REM between different developers that use different astyle versions as
-@REM different versions might have different output (this has happened in
-@REM the past).
+@ECHO off
+REM Script to run AStyle on the sources
+REM The version check in this script is used to avoid commit battles
+REM between different developers that use different astyle versions as
+REM different versions might have different output (this has happened in
+REM the past).
 
-@REM Get the directory of the script
+REM Get the directory of the script
 SET SCRIPT_DIR=%~dp0
 
-@REM Change to that directory
+REM Change to that directory
 CD /d %SCRIPT_DIR%
 
-@REM To require a newer astyle version, update ASTYLE_VERSION below.
-@REM ASTYLE_VERSION_STR is then constructed to match the beginning of the
-@REM version string reported by "astyle --version".
-@SET ASTYLE_VERSION="3.0.1"
-@SET ASTYLE_VERSION_STR="Artistic Style Version %ASTYLE_VERSION%"
-@SET ASTYLE="astyle"
+REM To require a newer astyle version, update ASTYLE_VERSION below.
+REM ASTYLE_VERSION_STR is then constructed to match the beginning of the
+REM version string reported by "astyle --version".
+SET ASTYLE_VERSION="3.0.1"
+SET ASTYLE_VERSION_STR="Artistic Style Version %ASTYLE_VERSION%"
+SET ASTYLE="astyle"
 
-@SET DETECTED_VERSION_STR=""
-@FOR /F "tokens=*" %%i IN ('%ASTYLE% --version') DO SET DETECTED_VERSION_STR=%%i
-@ECHO %DETECTED_VERSION_STR% | FINDSTR /B /C:%ASTYLE_VERSION_STR% > nul && (
+SET DETECTED_VERSION_STR=""
+FOR /F "tokens=*" %%i IN ('%ASTYLE% --version') DO SET DETECTED_VERSION_STR=%%i
+ECHO %DETECTED_VERSION_STR% | FINDSTR /B /C:%ASTYLE_VERSION_STR% > nul && (
     ECHO "%DETECTED_VERSION_STR%" matches %ASTYLE_VERSION_STR%
 ) || (
     ECHO You should use: %ASTYLE_VERSION_STR%
