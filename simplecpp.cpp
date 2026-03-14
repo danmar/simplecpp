56022f748fea76b89911754ce0a0ad4bf56596b0
firewave <firewave@users.noreply.github.com>
2026-03-14 22:28:30 +0100

    fixed `-Wlifetime-safety-intra-tu-suggestions` Clang warnings

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -57,6 +57,16 @@
 #  include <sys/stat.h>
 #endif
 
+#if defined(__has_cpp_attribute)
+#  if __has_cpp_attribute (clang::lifetimebound)
+#    define SIMPLECPP_LIFETIMEBOUND [[clang::lifetimebound]]
+#  else
+#    define SIMPLECPP_LIFETIMEBOUND
+#  endif
+#else
+#  define SIMPLECPP_LIFETIMEBOUND
+#endif
+
 static bool isHex(const std::string &s)
 {
     return s.size()>2 && (s.compare(0,2,"0x")==0 || s.compare(0,2,"0X")==0);
@@ -1679,7 +1689,7 @@ namespace simplecpp {
         }
 
         /** how has this macro been used so far */
-        const std::list<Location> &usage() const {
+        const std::list<Location> &usage() const SIMPLECPP_LIFETIMEBOUND {
             return usageList;
         }
 
@@ -1869,7 +1879,7 @@ namespace simplecpp {
 
         const Token *appendTokens(TokenList &tokens,
                                   const Location &rawloc,
-                                  const Token * const lpar,
+                                  const Token * const lpar SIMPLECPP_LIFETIMEBOUND,
                                   const MacroMap &macros,
                                   const std::set<TokenString> &expandedmacros,
                                   const std::vector<const Token*> &parametertokens) const {
@@ -2999,7 +3009,7 @@ static long long evaluate(simplecpp::TokenList &expr, const simplecpp::DUI &dui,
     return expr.cfront() && expr.cfront() == expr.cback() && expr.cfront()->number ? stringToLL(expr.cfront()->str()) : 0LL;
 }
 
-static const simplecpp::Token *gotoNextLine(const simplecpp::Token *tok)
+static const simplecpp::Token *gotoNextLine(const simplecpp::Token *tok SIMPLECPP_LIFETIMEBOUND)
 {
     const unsigned int line = tok->location.line;
     const unsigned int file = tok->location.fileIndex;
