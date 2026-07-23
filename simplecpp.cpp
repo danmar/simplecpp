23ab6e79d2ddf390f79a5de14b7b7e8c4c4714cd
glankk <glankk@users.noreply.github.com>
2026-07-23 11:42:25 +0200

    Update simplecpp.cpp
    Co-authored-by: Daniel Marjamäki <daniel.marjamaki@gmail.com>

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -767,7 +767,7 @@ void simplecpp::TokenList::readfile(Stream &stream, const std::string &filename,
                             msg += "Line number zero is undefined behavior.";
                         } else {
                             msg += "Line numbers above " + std::to_string(maxline) + " are " +
-                                   (cppstd == CPP26 ? "conditionally supported" : "undefined behavior") +
+                                   (cppstd >= CPP26 ? "conditionally supported" : "undefined behavior") +
                                    " in " + (std_is_c ? getCStdName(cstd) : getCppStdName(cppstd)) + ".";
                         }
                         simplecpp::Output err{
